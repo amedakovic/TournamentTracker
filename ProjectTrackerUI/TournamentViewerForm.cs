d69b65d7269a3da756541614279781df9f4e7b0a
amedakovic <medakovic.aleksa1@gmail.com>
2023-05-01 18:15:11 +0200

    Score error handling

--- ProjectTrackerUI/TournamentViewerForm.cs
+++ ProjectTrackerUI/TournamentViewerForm.cs
@@ -152,8 +152,42 @@ namespace ProjectTrackerUI
             LoadMatchups();
         }
 
+        private string ValidateData()
+        {
+            string output = "";
+            double teamOneScore;
+            double teamTwoScore;
+
+            bool scoreOneValid = double.TryParse(TeamOneScoreValue.Text, out teamOneScore);
+            bool scoreTwoValid = double.TryParse(TeamTwoScoreValue.Text, out teamTwoScore);
+
+            if(!scoreOneValid)
+            {
+                output ="The score one is not a valid number";
+            }
+            else if (!scoreTwoValid)
+            {
+                output = "The score two is not a valid number";
+            }
+            else if (teamOneScore == 0 && teamTwoScore == 0)
+            {
+                output="You did not enter a score for either team";
+            }
+            else if(teamOneScore == teamTwoScore)
+            {
+                output = "We do not allow ties in this application";
+            }
+            return output;
+        }
         private void scoreButton_Click(object sender, EventArgs e)
         {
+            string errorMessage = ValidateData();
+            if (errorMessage.Length > 0)
+            {
+                MessageBox.Show($"Input error: {errorMessage}");
+                return;
+            }
+
             MatchupModel m = (MatchupModel)MatchupListBox.SelectedItem;
             double teamOneScore = 0;
             double teamTwoScore = 0;
@@ -197,7 +231,15 @@ namespace ProjectTrackerUI
                     }
                 }
             }
-            TournamentLogic.UpdateTournamentResults(tournament);
+            try
+            {
+                TournamentLogic.UpdateTournamentResults(tournament);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"The application had the following error: {ex.Message}");
+                return;
+            }
 
             LoadMatchups();
 
